0d7f39a1c1b2932258cd23e3140b0d77cd963d69
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-04-17 17:14:16 +0100

    Done, but poem validator is weird

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,57 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) =>{
+    if (event.target.name === 'username'){
+      this.setState({
+        username: event.target.value
+      });
+    } else {
+      this.setState({
+        password: event.target.value
+      });
+    }
+    
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {username, password} = this.state;
+
+    if (!username || !password){
+      return;
+    }
+
+    this.props.onSubmit({username,password})
+
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" 
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" 
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
